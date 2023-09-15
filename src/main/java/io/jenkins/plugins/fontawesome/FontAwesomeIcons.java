70bece637c59d6b6dbc4f6eedad36d8dc2da399e
Ulli Hafner <ullrich.hafner@gmail.com>
2023-09-15 21:45:28 +0200

    Fix outstanding SpotBugs warnings.

--- src/main/java/io/jenkins/plugins/fontawesome/FontAwesomeIcons.java
+++ src/main/java/io/jenkins/plugins/fontawesome/FontAwesomeIcons.java
@@ -1,17 +1,29 @@
 package io.jenkins.plugins.fontawesome;
 
-import edu.umd.cs.findbugs.annotations.CheckForNull;
-import org.apache.commons.lang3.StringUtils;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.*;
-import java.util.*;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import io.jenkins.plugins.fontawesome.SvgTag.FontAwesomeStyle;
 
 /**
  * Utility to work with icons provided by the font-awesome-api-plugin.
@@ -19,7 +31,6 @@ import java.util.stream.Stream;
  * @author strangelookingnerd
  */
 public final class FontAwesomeIcons {
-
     private static final Logger LOGGER = Logger.getLogger(FontAwesomeIcons.class.getName());
     private static final String SVG_FILE_ENDING = ".svg";
     private static final String IMAGES_SYMBOLS_PATH = "images/symbols/";
@@ -29,7 +40,9 @@ public final class FontAwesomeIcons {
     /**
      * Takes an icon name and generates an icon class name from it.
      *
-     * @param icon the icon name
+     * @param icon
+     *         the icon name
+     *
      * @return the icon class name
      */
     public static String getIconClassName(final String icon) {
@@ -39,8 +52,7 @@ public final class FontAwesomeIcons {
     /**
      * Get all available icons provided by the font-awesome-api-plugin.
      *
-     * @return a sorted map of available icons with icon name as key and the icon
-     * class name as value.
+     * @return a sorted map of available icons with icon name as key and the icon class name as value.
      */
     public static Map<String, String> getAvailableIcons() {
         return getAvailableIcons(null);
@@ -49,15 +61,17 @@ public final class FontAwesomeIcons {
     /**
      * Get all available icons provided by the font-awesome-api-plugin filtered by their style.
      *
-     * @param filter the style to filter
-     * @return a sorted map of available icons with icon name as key and the icon
-     * class name as value filtered by the given style.
+     * @param filter
+     *         the style to filter
+     *
+     * @return a sorted map of available icons with icon name as key and the icon class name as value filtered by the
+     *         given style.
      */
-    public static Map<String, String> getAvailableIcons(final SvgTag.FontAwesomeStyle filter) {
+    public static Map<String, String> getAvailableIcons(@CheckForNull final FontAwesomeStyle filter) {
         return getIconsFromClasspath(filter);
     }
 
-    private static Map<String, String> getIconsFromClasspath(final SvgTag.FontAwesomeStyle filter) {
+    private static Map<String, String> getIconsFromClasspath(@CheckForNull final FontAwesomeStyle filter) {
         try {
             Enumeration<URL> urls = FontAwesomeIcons.class.getClassLoader().getResources(IMAGES_SYMBOLS_PATH);
 
@@ -85,18 +99,24 @@ public final class FontAwesomeIcons {
         return new LinkedHashMap<>();
     }
 
-    private static Map<String, String> collectIcons(final Path path,  @CheckForNull final SvgTag.FontAwesomeStyle filter) throws IOException {
+    @SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
+    private static Map<String, String> collectIcons(final Path path, @CheckForNull final FontAwesomeStyle filter)
+            throws IOException {
         Map<String, String> icons = new LinkedHashMap<>();
 
         if (path != null) {
-            try (Stream<Path> stream = filter == null ? Files.walk(path, 2) : Files.walk(path.resolve(filter.name().toLowerCase(Locale.ENGLISH)), 1)) {
-                stream.filter(icon -> icon != null && icon.getFileName() != null && StringUtils.endsWith(icon.getFileName().toString(), SVG_FILE_ENDING))
+            try (Stream<Path> stream = filter == null ? Files.walk(path, 2) : Files.walk(
+                    path.resolve(filter.name().toLowerCase(Locale.ENGLISH)), 1)) {
+                stream.filter(icon -> icon != null && icon.getFileName() != null && StringUtils.endsWith(
+                                icon.getFileName().toString(), SVG_FILE_ENDING))
                         .sorted().forEach(icon -> {
-                            if (icon.getParent() != null && icon.getParent().getFileName() != null && icon.getFileName() != null) {
-                                String iconName = icon.getParent().getFileName() + "/" + StringUtils.removeEnd(icon.getFileName().toString(), SVG_FILE_ENDING);
-                                icons.put(iconName, getIconClassName(iconName));
-                            }
-                        }
+                                    if (icon.getParent() != null && icon.getParent().getFileName() != null
+                                            && icon.getFileName() != null) {
+                                        String iconName = icon.getParent().getFileName() + "/" + StringUtils.removeEnd(
+                                                icon.getFileName().toString(), SVG_FILE_ENDING);
+                                        icons.put(iconName, getIconClassName(iconName));
+                                    }
+                                }
                         );
             }
         }
@@ -107,5 +127,4 @@ public final class FontAwesomeIcons {
     private FontAwesomeIcons() {
         // hidden
     }
-
 }
