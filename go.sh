37eecced20264161996f9487502eb289d10358ce
Ulli Hafner <ullrich.hafner@gmail.com>
2024-08-10 18:11:48 +0200

    Fix docker compose call.

--- go.sh
+++ go.sh
@@ -13,8 +13,8 @@ cp -fv target/font-awesome-api.hpi $JENKINS_HOME/plugins/font-awesome-api.jpi
 
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
-IS_RUNNING=$(docker-compose ps -q jenkins-controller)
+IS_RUNNING=$(docker compose ps -q devenv-jenkins)
 if [[ "$IS_RUNNING" != "" ]]; then
-    docker-compose restart
+    docker compose restart
     echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
 fi
